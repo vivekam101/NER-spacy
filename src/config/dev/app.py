05f8680f99e1fb6cf681c6e97b7930d84c6b4f22
Sujith Abdul Rahim <sujiar37@gmail.com>
2020-05-01 05:27:55 +0530

    env , pip3 & Dockerfile_ML

--- src/config/dev/app.py
+++ src/config/dev/app.py
@@ -8,13 +8,13 @@ from config.common import DefaultConfig
 
 class Development(DefaultConfig):
     def __init__(self):
-        self.ce_host = os.environ.get('CE_PREDICT_SKILL_HOST')
-        self.ce_port = int(os.environ.get('CE_PREDICT_SKILL_PORT'))
+        self.ce_host = os.environ.get('CE_PREDICT_HOST')
+        self.ce_port = int(os.environ.get('CE_PREDICT_PORT'))
 
         # model parameters
-        self.model_path = os.environ.get('CE_PREDICT_SKILL_MODEL_PATH')
+        self.model_path = os.environ.get('CE_PREDICT_MODEL_PATH')
 
         # log parameters
-        self.log_path = os.environ.get('CE_PREDICT_SKILL_LOG_PATH')
-        self.log_level = os.environ.get('CE_PREDICT_SKILL_LOG_LEVEL')
+        self.log_path = os.environ.get('CE_PREDICT_LOG_PATH')
+        self.log_level = os.environ.get('CE_PREDICT_LOG_LEVEL')
         self.log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
