aed942d65e80db15211e01800065c79994bca929
Sujith Abdul Rahim <sujiar37@gmail.com>
2020-04-30 14:14:59 +0530

    restructured

--- config/common.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#    Copyright (C) 2019-TODAY Cleareye.ai(<https://www.cleareye.ai>)
-#    Author: Cleareye.ai(<https://www.cleareye.ai>)
-
-
-class DefaultConfig(object):
-    debug = False
-    testing = False
